adbea6379dafa1e2fcf971c86198c06c34755fea
Jinendra Jain <jjinendra3@gmail.com>
2024-05-04 21:56:08 +0530

    basic strucuture done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,89 @@
+"use client";
 import Image from "next/image";
+import { CiBookmark } from "react-icons/ci";
+import { FaBookmark } from "react-icons/fa";
+import { useState } from "react";
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+import { FaArrowLeft,FaArrowRight } from "react-icons/fa6";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+import ques from "../../public/ques.png";
 
 export default function Home() {
+  const [value, setValue] = useState<string | undefined>(undefined);
+  const handleValue = (value: string) => {
+    setValue(value);
+  };
+  const [bookmark, setBookmark] = useState<boolean>(false);
+  console.log(value!=undefined?value.slice(0,1):"");
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex flex-col min-h-screen p-6 border-4 border-gray-300">
+      <div className="flex items-center space-x-2 text-2xl font-bold mb-4 rounded-xl">
+        <h1>Question 1</h1>
+        {bookmark ? (
+          <FaBookmark onClick={() => setBookmark(false)} />
+        ) : (
+          <CiBookmark onClick={() => setBookmark(true)} />
+        )}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <p className="mb-4 font-semibold">
+        The ratio of a cylinder to that of a cone is 2:5. Find the ratio of
+        volume of the cylinder to that of a cone. The ratio of the radius of a
+        cylinder to that of a cone is 2:5. Find the ratio of volume of the
+        cylinder to that of a cone.
+      </p>
+      <div className="mb-4">
+        <Image src={ques} alt="Question Image" className="h-64 w-64 ml-3" />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <RadioGroup onValueChange={handleValue} className="flex flex-wrap flex-col w-full md:space-y-6 space-y-2">
+  <div className="flex flex-col md:flex-row md:space-x-6 ">
+    <div className={`flex space-x-2 py-3 px-5 md:w-96  items-center rounded-md ${value!==undefined ? (value.slice(0,1)==="A"?'bg-blue-300':'bg-white'):'bg-white'} shadow-xl border mb-4 md:mb-0`}>
+      <RadioGroupItem value="A. Chemistry" id="r1" />
+      <Label htmlFor="r1">A. Chemistry</Label>
+    </div>
+    <div className={`flex space-x-2 py-3 px-5 md:w-96 items-center rounded-md ${value!==undefined? (value.slice(0,1)==="B"?'bg-blue-300':'bg-white'):'bg-white'} shadow-xl border`}>
+      <RadioGroupItem value="B. Chemistry" id="r2" />
+      <Label htmlFor="r2">B. Chemistry</Label>
+    </div>
+  </div>
+  <div className="flex flex-col md:flex-row md:space-x-6 ">
+    <div className={`flex space-x-2 py-3 px-5 md:w-96 items-center rounded-md ${value!==undefined? (value.slice(0,1)==="C"?'bg-blue-300':'bg-white'):'bg-white'} shadow-xl border mb-4 md:mb-0`} >
+      <RadioGroupItem value="C. Chemistry" id="r3" />
+      <Label htmlFor="r3">C. Chemistry</Label>
+    </div>
+    <div className={`flex space-x-2 py-3 px-5 md:w-96 items-center rounded-md ${value!=undefined?(value.slice(0,1)==="D"?'bg-blue-300':'bg-white'):'bg-white'} shadow-xl border`}>
+      <RadioGroupItem value="D. Chemistry" id="r4" />
+      <Label htmlFor="r4">D. Chemistry</Label>
+    </div>
+  </div>
+</RadioGroup>
+<div className="md:flex md:flex-row md:justify-between md:mt-auto text-gray-500 mt-4">
+  <Button variant="outline" className="flex space-x-2 items-center ">
+    <FaArrowLeft />
+    <div className="font-bold hidden md:block">Previous</div>
+  </Button>
+  <div className="space-x-4">
+    <Button variant="outline" className="font-bold py-2">
+      Check Solution
+    </Button>
+    <Button variant="outline" className="font-bold  bg-blue-600 text-white">
+      Submit Answer
+    </Button>
+  </div>
+  <Button variant="outline" className="flex space-x-2 items-center">
+    <div className="font-bold hidden md:block">Skip</div>
+    <FaArrowRight />
+  </Button>
+</div>
+    </div>
   );
 }
